2632e6b387a9c3e21e87d240ee2a57fb00c3df9e
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2025-08-23 09:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def download_pdf():
 # Run App
 # ===============================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
